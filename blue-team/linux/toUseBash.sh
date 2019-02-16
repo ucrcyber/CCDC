71c6ad5442b8589eb88fe1c3f0aabb5a95499850
Anthony <ahall012@ucr.edu>
2019-02-16 08:45:27 -0800

    script is now bash focused and the TCSH version still needs syntax work

--- blue-team/linux/toUseBash.sh
+++ blue-team/linux/toUseBash.sh
@@ -1,60 +1,10 @@
 #!/usr/bin/env bash
 
-# Check if user is root
 if [[ $EUID -ne 0 ]]; then
 	printf "\nScript must be run as root\n"
 	exit 1
 fi
 
-# /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// #
-# /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// #
-# /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// #
-
-# This script is supposed to automate a lot of the beginning steps on the linux side
-
-# /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// #
-# /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// #
-# /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// #
-
-# /// TODO:
-# /// [[ doneForDebian/Ubuntu ]] install OSSEC (client & server),
-# /// ELK/Splunk
-# /// (delay) Replace Sources.list with defaults for distros to automate the auditing process
-# /// Turn off all SystemD/init services that are not part of system (WATCH OUT FOR WEBSERVERS & SUCH!)
-# /// [[ DONE? ]] default iptables rules (block ssh, ipv6, icmp, ddos, etc)
-# /// [[ DONE? ]] Edit sshd configs and such
-# /// [[ seperate? ]] Ansible playbook
-# /// FTP/DNS security
-# /// [[ doneForCentOS/RHEL/Fedora) ]] modsecurity
-# /// replace default rc files with safe ones, ie bashrc with a default with secure aliases and such
-# /// check var spool for cron as well as etc
-# /// change wordpress passwords
-# /// change db passwords
-# /// nesses
-
-# /// watch out for
-# /// automated scans, stop user agents (hi im firefox)
-# /// use a local scanner () to find and repair our stuff before hand
-# /// check permissions on web to stop
-# /// look up BloodHound (windows),
-
-# /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// #
-# /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// #
-# /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// #
-
-# /// DISTROS TO DO
-# /// Mint/Ubuntu/Debian, CentOS/RHEL/Fedora,
-# /// Alpine, Arch, Gentoo, elementaryOS,
-# /// Manjaro, ?KDE Neon?, Void linux, OpenSUSE, ?BSD?
-# /// MAYBE?: BSD jails, Container Hells, Dockers
-
-# /// know basic services: POSTFIX, bind, ftp?, dns?, sambashare, apache
-
-# /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// #
-# /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// #
-# /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// #
-
-#loops so the user can do multiple options after each others
 while [ true ]
 do
 
@@ -69,24 +19,15 @@ printf 'Option 7 : default iptables & chattr +i to backup\n'
 printf 'Option 8 : Secures /etc/ssh/ssh_config and chattr +i\n'
 printf 'Option 9 : Change permissions on /root /home/* to only be for root & the respective owner of home*\n'
 printf 'Option 10 : (ONLY CENTOS) install mod_security\n'
-printf 'Option 11 : setup Ansible client/server\n'
-printf 'Option 12 : Secure PostFix mail\n'
-printf 'Option 13 : nmap -A -T4 192.168.1.0/24 >> nmapScan.txt\n'
-
-printf '[[ TODO ]] Option 14 : default rc files & chattr +i for nano/vi/vim/bash/zsh/fsh/etc ...\n'
-printf '[[ TODO ]] Option 15 : audit SystemD services\n'
-printf '[[ TODO ]] Option 16 : domain join linux\n'
-printf '[[ TODO ]] Option 17 : Remove GCC, NC\n'
-printf ' (delay) [[ TODO ]] Option 18 : audit sources/mirrors\n'
+printf 'Option 11 : Setup Ansible client/server\n'
+printf 'Option 12 : Secures PostFix\n'
+printf 'Option 13 : nmap -A -T4 192.168.1.0/24 >> nmapScan.txt &\n'
 read -r option
 
 if [[ -z "$option" ]]; then
 	printf '\nInvalid option\n'
 fi
 
-
-
-#change passwords
 if [[ "$option" = "1" ]]; then
 	printf '\nEnter the new password template: '
 	read -r passTemp
@@ -98,9 +39,8 @@ if [[ "$option" = "1" ]]; then
 		exit 2
 	fi
 
-# host=$(hostname)
 	for user in $(awk -F: '$7 ~ /(\/sh|\/bash)/ { print $1 }' /etc/passwd); do
-		if [[ $(id -u $user) -eq 0 ]]; then # Don't change password for root
+		if [[ $(id -u $user) -eq 0 ]]; then
 			continue
 		fi
 		echo ""
@@ -112,26 +52,18 @@ if [[ "$option" = "1" ]]; then
 	printf '\nPASSWORDS CHANGED!\n'
 fi
 
-
-
-
-#disables ipv6
 if [[ "$option" = "2" ]]; then
 	if [ ! -f /etc/sysctl.conf ]; then
 		echo "\nSorry! No /etc/sysctl.conf exists!"
 	else
 		echo 'net.ipv6.conf.all.disable_ipv6 = 1' >> /etc/sysctl.conf
 		echo 'net.ipv6.conf.default.disable_ipv6 = 1' >> /etc/sysctl.conf
-		#cat /etc/sysctl.conf
 		printf '\nRunning sysctl -p /etc/sysctl.conf\n'
 		sysctl -p /etc/sysctl.conf
 		printf '\nipv6 DISABLED!\n'
 	fi
 fi
 
-
-
-#chattr +i shadow,sudoers,group
 if [[ "$option" = "3" ]]; then
 	printf '\nDid you audit the files first? (Y/N)\n'
 	read -r option
@@ -153,9 +85,6 @@ if [[ "$option" = "3" ]]; then
 		printf '\nLocking /etc/cron* /etc/anacrontab'
 		chattr +i /etc/anacrontab && chattr -R +i /etc/cron.d && chattr -R +i /etc/cron.daily && chattr -R +i /etc/cron.hourly && chattr -R +i /etc/cron.monthly && chattr +i /etc/crontab && chattr -R +i /etc/cron.weekly
 
-		#check distrib to +i to sources
-		#TODO: SystemD services
-
 		printf '\n\nLook! They are changed!\n'
 		lsattr /etc/group /etc/shadow /etc/gshadow /etc/sudoers /etc/sudoers.d /etc/sysctl.conf /etc/sysctl.d /etc/anacrontab /etc/cron*
 		printf '\nDone\n'
@@ -167,9 +96,6 @@ if [[ "$option" = "3" ]]; then
 
 fi
 
-
-
-#chattr -i shadow,sudoers,group
 if [[ "$option" = "4" ]]; then
 	printf '\nUnlocking /etc/shadow /etc/gshadow'
 	chattr -i /etc/shadow && chattr -i /etc/gshadow
@@ -189,19 +115,15 @@ if [[ "$option" = "4" ]]; then
 	printf '\nDone\n'
 fi
 
-
-
-#install OSSEC / ELK
 if [[ "$option" = "5" ]]; then
 	printf '\nPrereq: download & install build-essentials, inotify-tools\n'
 	ID_LIKE="$(cat /etc/*release | grep "ID_LIKE")"
 
-	printf '\nDo you want [E]LK or [S]plunk? or [N]either?\n'
+	printf '\nDo you want [E]LK (not finished) or [S]plunk (not finished)? or [N]either (PICK THIS ONE!!) ?\n'
 	read -r optionEorS
 
-	#checks for distros
 	if [[ ${ID_LIKE} = "ID_LIKE=debian" ]] || [[ ${ID_LIKE} = "ID_LIKE=\"debian\"" ]]; then
-		apt-get install build-essential inotify-tools wget
+		apt-get -y install build-essential inotify-tools wget git
 		wget -U ossec https://bintray.com/artifact/download/ossec/ossec-hids/ossec-hids-2.8.3.tar.gz
 		tar -xvzf ossec-hids-2.8.3.tar.gz
 		cd ossec-hids-2.8.3
@@ -217,7 +139,6 @@ if [[ "$option" = "5" ]]; then
 		echo "To set up mail, get alerts and change frequency of scans, go to /var/ossec/etc/ossec.conf"
 		echo "To make your own rules, and severiry of broken rules, go to /var/ossec/rules/local_rules.xml"
 
-		#setup Elk
 		if [[ "$optionEorS" = "E" ]] || [[ "$optionEorS" = "e" ]] || [[ "$optionEorS" = "ELK" ]] || [[ "$optionEorS" = "elk" ]] || [[ "$optionEorS" = "Elk" ]]; then
 			printf '\nPrereq: java8\n'
 			add-apt-repository -y ppa:webupd8team/java
@@ -237,13 +158,13 @@ if [[ "$option" = "5" ]]; then
 			echo "deb http://packages.elastic.co/kibana/4.5/debian stable main" | sudo tee -a /etc/apt/sources.list.d/kibana-4.5.x.list
 			apt-get update
 			apt-get -y install kibana
-			vi /opt/kibana/config/kibana.yml #See the linux google doc for what to add
+			vi /opt/kibana/config/kibana.yml
 			update-rc.d kibana defaults 96 9
 			service kibana start
 
 			printf '\nTime for Nginx!\n'
 			apt-get install nginx apache2-utils
-			htpasswd -c /etc/nginx/htpasswd.users jeff #jeff is the kibanaadmin name
+			htpasswd -c /etc/nginx/htpasswd.users jeff
 			rm -rf /etc/nginx/sites-available/default
 			touch /etc/nginx/sites-available/default
 			printf '\nWhat is the server_name? (example.com)\n'
@@ -273,15 +194,14 @@ if [[ "$option" = "5" ]]; then
 			echo 'deb http://packages.elastic.co/logstash/2.2/debian stable main' | sudo tee /etc/apt/sources.list.d/logstash-2.2.x.list
 			apt-get update
 			apt-get install logstash
-			#Generate SSL Certificates and use IP address instead of DNS
 			mkdir -p /etc/pki/tls/certs
 			mkdir /etc/pki/tls/private
-			vi /etc/ssl/openssl.cnf #See the linux google doc for what to add
+			vi /etc/ssl/openssl.cnf
 			cd /etc/pki/tls
 			sudo openssl req -config /etc/ssl/openssl.cnf -x509 -days 3650 -batch -nodes -newkey rsa:2048 -keyout private/logstash-forwarder.key -out certs/logstash-forwarder.crt
-			vi /etc/logstash/conf.d/02-beats-input.conf #See the linux google doc for what to add
-			vi /etc/logstash/conf.d/10-syslog-filter.conf #See the linux google doc for what to add
-			vi /etc/logstash/conf.d/30-elasticsearch-output.conf #See the linux google doc for what to add
+			vi /etc/logstash/conf.d/02-beats-input.conf
+			vi /etc/logstash/conf.d/10-syslog-filter.conf
+			vi /etc/logstash/conf.d/30-elasticsearch-output.conf
 			service logstash configtest
 			service logstash restart
 			update-rc.d logstash defaults 96 9
@@ -308,12 +228,10 @@ if [[ "$option" = "5" ]]; then
 
 		fi
 
-		#setup Splunk
 		if [[ "$optionEorS" = "S" ]] || [[ "$optionEorS" = "s" ]] || [[ "$optionEorS" = "Splunk" ]] || [[ "$optionEorS" = "splunk" ]]; then
 			printf '\n[[TODO]]\n'
 		fi
 
-		#user wants neither
 		if [[ "$optionEorS" = "N" ]] || [[ "$optionEorS" = "n" ]] || [[ "$optionEorS" = "No" ]] || [[ "$optionEorS" = "no" ]] || [[ "$optionEorS" = "Neither" ]] || [[ "$optionEorS" = "neither" ]]; then
 			printf '\n[[TODO]]\n'
 		fi
@@ -322,11 +240,8 @@ if [[ "$option" = "5" ]]; then
 
 	if [[ ${ID_LIKE} = "ID_LIKE=arch" ]] || [[ ${ID_LIKE} = "ID_LIKE=\"arch\"" ]]; then
 		printf '\nDone, but needs testing, WARNING!\n'
-		# commands from the arch wiki, I think I figured it out
-		# but an arch user needs to approve the following
-		# or we can test it
 
-		pacman -S build-essential inotfify-tools wget
+		pacman -S build-essential inotfify-tools wget git
 		wget -U ossec https://bintray.com/artifact/download/ossec/ossec-hids/ossec-hids-2.8.3.tar.gz
 		tar -xvzf ossec-hids-2.8.3.tar.gz
 		cd ossec-hids-2.8.3
@@ -342,18 +257,14 @@ if [[ "$option" = "5" ]]; then
 		echo "To set up mail, get alerts and change frequency of scans, go to /var/ossec/etc/ossec.conf"
 		echo "To make your own rules, and severiry of broken rules, go to /var/ossec/rules/local_rules.xml"
 
-
-		#setup Elk
 		if [[ "$optionEorS" = "E" ]] || [[ "$optionEorS" = "e" ]] || [[ "$optionEorS" = "ELK" ]] || [[ "$optionEorS" = "elk" ]] || [[ "$optionEorS" = "Elk" ]]; then
 			printf '\n[[TODO]]\n'
 		fi
 
-		#setup Splunk
 		if [[ "$optionEorS" = "S" ]] || [[ "$optionEorS" = "s" ]] || [[ "$optionEorS" = "Splunk" ]] || [[ "$optionEorS" = "splunk" ]]; then
 			printf '\n[[TODO]]\n'
 		fi
 
-		#user wants neither
 		if [[ "$optionEorS" = "N" ]] || [[ "$optionEorS" = "n" ]] || [[ "$optionEorS" = "No" ]] || [[ "$optionEorS" = "no" ]] || [[ "$optionEorS" = "Neither" ]] || [[ "$optionEorS" = "neither" ]]; then
 			printf '\n[[TODO]]\n'
 		fi
@@ -362,12 +273,11 @@ if [[ "$option" = "5" ]]; then
 
 	if [[ ${ID_LIKE} = "ID_LIKE=rhel fedora" ]] || [[ ${ID_LIKE} = "ID_LIKE=\"rhel fedora\"" ]]; then
 		printf '\nDone, but needs testing, WARNING!\n'
-		# commands from stackExchange, I think I figured it out
-		# but we need to test it
 
 		yum groupinstall 'Development Tools'
 		yum install inotify-tools
 		yum install wget
+		yum install mod_security
 		wget -U ossec https://bintray.com/artifact/download/ossec/ossec-hids/ossec-hids-2.8.3.tar.gz
 		tar -xvzf ossec-hids-2.8.3.tar.gz
 		cd ossec-hids-2.8.3
@@ -383,17 +293,14 @@ if [[ "$option" = "5" ]]; then
 		echo "To set up mail, get alerts and change frequency of scans, go to /var/ossec/etc/ossec.conf"
 		echo "To make your own rules, and severiry of broken rules, go to /var/ossec/rules/local_rules.xml"
 
-		#setup Elk
 		if [[ "$optionEorS" = "E" ]] || [[ "$optionEorS" = "e" ]] || [[ "$optionEorS" = "ELK" ]] || [[ "$optionEorS" = "elk" ]] || [[ "$optionEorS" = "Elk" ]]; then
 			printf '\n[[TODO]]\n'
 		fi
 
-		#setup Splunk
 		if [[ "$optionEorS" = "S" ]] || [[ "$optionEorS" = "s" ]] || [[ "$optionEorS" = "Splunk" ]] || [[ "$optionEorS" = "splunk" ]]; then
 			printf '\n[[TODO]]\n'
 		fi
 
-		#user wants neither
 		if [[ "$optionEorS" = "N" ]] || [[ "$optionEorS" = "n" ]] || [[ "$optionEorS" = "No" ]] || [[ "$optionEorS" = "no" ]] || [[ "$optionEorS" = "Neither" ]] || [[ "$optionEorS" = "neither" ]]; then
 			printf '\n[[TODO]]\n'
 		fi
@@ -402,9 +309,6 @@ if [[ "$option" = "5" ]]; then
 
 fi
 
-
-
-#Update and Upgrade system
 if [[ "$option" = "6" ]]; then
 	ID_LIKE="$(cat /etc/*release | grep "ID_LIKE")"
 	printf '\nDid you audit the source files first? (Y/N)\n'
@@ -434,9 +338,6 @@ if [[ "$option" = "6" ]]; then
 	fi
 fi
 
-
-
-#Does some basic Network Binder setup stuff
 if [[ "$option" = "7" ]]; then
 	printf '\nSaving iptables to ~/iptablesBackupBefore.txt . . .\n'
 	touch ~/iptablesBackupBefore.txt
@@ -465,41 +366,34 @@ if [[ "$option" = "7" ]]; then
 	chattr +i ~/iptablesBackupAfter.txt
 fi
 
-
-
-#ssh configs
 if [[ "$option" = "8" ]]; then
 	echo "
-	Protocol 2
-	AllowUsers root admin webmaster
-	AllowGroup sshusers
-	PasswordAuthentication no
-	HostbasedAuthentication no
-	RSAAuthentication yes
-	PubkeyAuthentication yes
-	PermitEmptyPasswords no
-	PermitRootLogin no
-	ServerKeyBits 2048
-	IgnoreRhosts yes
-	RhostsAuthentication no
-	RhostsRSAAuthentication no" >> /etc/ssh/ssh_config
+Protocol 2
+AllowUsers root admin webmaster
+AllowGroup sshusers
+PasswordAuthentication no
+HostbasedAuthentication no
+RSAAuthentication yes
+PubkeyAuthentication yes
+PermitEmptyPasswords no
+PermitRootLogin no
+ServerKeyBits 2048
+IgnoreRhosts yes
+RhostsAuthentication no
+RhostsRSAAuthentication no" >> /etc/ssh/ssh_config
 
 	chattr +i /etc/ssh/ssh_config
 	lsattr /etc/ssh/*
 	printf '\nDone\n'
 fi
 
-
-
-#change permissions to only be root
 if [[ "$option" = "9" ]]; then
-	printf '\nChanging Permissions on /root & /home/* . . .\n'
-	chmod -Rv go-rwx /root
-	chmod -Rv go-w /home/*
+	printf '\nChanging Permissions on /root & /home/* . . . \n'
+	chmod -Rv go-rwx /root &
+	chmod -Rv go-w /home/* &
+	printf '\nThe time to finish will depend on the amount of users and files,\nthe task is running in the background so it will finish without telling you\n'
 fi
 
-
-#install mod_security
 if [[ "$option" = "10" ]]; then
 	ID_LIKE="$(cat /etc/*release | grep "ID_LIKE")"
 
@@ -524,10 +418,6 @@ if [[ "$option" = "10" ]]; then
 
 fi
 
-
-
-
-#setup Ansible client/server
 if [[ "$option" = "11" ]]; then
 	ID_LIKE="$(cat /etc/*release | grep "ID_LIKE")"
 	printf '\n[C]lient or [S]erver?\n'
@@ -541,6 +431,7 @@ if [[ "$option" = "11" ]]; then
 			apt-get install software-properties-common
 	 		apt-add-repository ppa:ansible/ansible
 			apt-get update
+			apt-get install python
 			apt-get install ansible
 			printf '\nAll set! Be sure to edit the /etc/ansible/hosts file and to place the ssh key from the server in ~/.ssh/authorized_keys\n'
 			chattr +i /etc/apt/*
@@ -560,11 +451,12 @@ if [[ "$option" = "11" ]]; then
 		fi
 	fi
 
-	if [[ ${ID_LIKE} = "ID_LIKE=rhel fedora" ]] || [[ ${ID_LIKE} = "ID_LIKE=\"rhel fedora\"" ]];
+	if [[ ${ID_LIKE} = "ID_LIKE=rhel fedora" ]] || [[ ${ID_LIKE} = "ID_LIKE=\"rhel fedora\"" ]]; then
 		if [[ "$optionCorS" = "C" ]] || [[ "$optionCorS" = "c" ]] || [[ "$optionCorS" = "Client" ]] || [[ "$optionCorS" = "client" ]] || [[ "$optionCorS" = "CLIENT" ]]; then
 			printf '\nInstalling Ansible\n'
 			yum install epel-release
 			yum update
+			yum install python
 			yum install ansible
 			printf '\nAll set! Be sure to edit the /etc/ansible/hosts file and to place the ssh key from the server in ~/.ssh/authorized_keys\n'
 		fi
@@ -573,11 +465,12 @@ if [[ "$option" = "11" ]]; then
 			printf '\nInstalling Ansible\n'
 			yum install epel-release
 			yum update
+			yum install python
 			yum install ansible
 			ssh-keygen
 			printf '\nAll set! Be sure to edit the /etc/ansible/hosts file and to place the PUBLIC ssh key from the server in ~/.ssh/authorized_keys on the clients\n'
 		fi
-	fi
+fi
 
 	if [[ ${ID_LIKE} = "ID_LIKE=arch" ]] || [[ ${ID_LIKE} = "ID_LIKE=\"arch\"" ]]; then
 		if [[ "$optionCorS" = "C" ]] || [[ "$optionCorS" = "c" ]] || [[ "$optionCorS" = "Client" ]] || [[ "$optionCorS" = "client" ]] || [[ "$optionCorS" = "CLIENT" ]]; then
@@ -594,46 +487,34 @@ if [[ "$option" = "11" ]]; then
 			printf '\nAll set! Be sure to edit the /etc/ansible/hosts file and to place the PUBLIC ssh key from the server in ~/.ssh/authorized_keys on the clients\n'
 		fi
 	fi
-
 fi
 
-
-
-
-#Secures PostFix
 if [[ "$option" = "12" ]]; then
-	printf '\nMake sure Postfix is running as NON-ROOT!!\n'
-	ps aux | grep postfix | grep -v '^root'
-	printf '\nIf it is NOT root say \"Y\" otherwise say \"N\"\n'
-	read -r isPostfixRoot
+	if [[ ! -f /etc/postfix/* ]]; then
+		printf "\nSorry! No PostFix files found!\n"
+	else
+		printf '\nMake sure Postfix is running as NON-ROOT!!\n'
+		ps aux | grep postfix | grep -v '^root'
+		printf '\nIf it is NOT root say \"Y\" otherwise say \"N\"\n'
+		read -r isPostfixRoot
 
-	if [[ "$isPostfixRoot" = "Y" ]] || [[ "$isPostfixRoot" = "y" ]]; then
-		printf '\nGreat!\nTime to change permissions'
-		chmod 755 /etc/postfix
-		chmod 644 /etc/postfix/*.cf
-		chmod 755 /etc/postfix/postfix-script*
-		chmod 755 /var/spool/postfix
-		chown root:root /var/log/mail*
-		chmod 600 /var/log/mail*
-		#printf '\nWhat is the mail.server.example.com?\n'
-		#read -r myServer
-		#printf '\nWhat is the IP adress on the NIC postfix should listen on?\n'
-		#read -r myNICIP
-		#sed -i 's/inet_interfaces = all/inet_interfaces = $myNICIP/' /etc/postfix/main.cf
-		vi /etc/postfix/main.cf # check linux google doc on what to add
-		service postfix restart
+		if [[ "$isPostfixRoot" = "Y" ]] || [[ "$isPostfixRoot" = "y" ]]; then
+			printf '\nGreat!\nTime to change permissions'
+			chmod 755 /etc/postfix
+			chmod 644 /etc/postfix/*.cf
+			chmod 755 /etc/postfix/postfix-script*
+			chmod 755 /var/spool/postfix
+			chown root:root /var/log/mail*
+			chmod 600 /var/log/mail*
+			vi /etc/postfix/main.cf
+			service postfix restart
+		fi
+
+		if [[ "$isPostfixRoot" = "N" ]] || [[ "$isPostfixRoot" = "n" ]]; then
+			printf '\nSince it is not root, kill it and start as root\n'
+		fi
 	fi
-
-	if [[ "$isPostfixRoot" = "N" ]] || [[ "$isPostfixRoot" = "n" ]]; then
-		printf '\nSince it is not root, kill it and start as root\n'
-	fi
-
 fi
-
-
-
-
-
 
 if [[ "$option" = "13" ]]; then
 	ID_LIKE="$(cat /etc/*release | grep "ID_LIKE")"
@@ -658,70 +539,4 @@ if [[ "$option" = "13" ]]; then
 	printf '\nThe nmap scan is now running in the background! Check for the txt in about 2 min!'
 fi
 
-
-
-
-
-#default rc files
-if [[ "$option" = "14" ]]; then
-	printf '\nTODO\n'
-fi
-
-
-
-
-#audit SystemD services
-if [[ "$option" = "15" ]]; then
-	printf '\nTODO\n'
-fi
-
-
-
-
-
-#domain join linux to windows AD and stuff
-if [[ "$option" = "16" ]]; then
-	printf '\nTODO\n'
-fi
-
-
-
-
-#Removes GCC, NC
-if [[ "$option" = "17" ]]; then
-	if [[ ${ID_LIKE} = "ID_LIKE=rhel fedora" ]] || [[ ${ID_LIKE} = "ID_LIKE=\"rhel fedora\"" ]]; then
-		printf '\nTODO\n'
-	fi
-
-	if [[ ${ID_LIKE} = "ID_LIKE=arch" ]] || [[ ${ID_LIKE} = "ID_LIKE=\"arch\"" ]]; then
-		printf '\nTODO\n'
-	fi
-
-	if [[ ${ID_LIKE} = "ID_LIKE=debian" ]] || [[ ${ID_LIKE} = "ID_LIKE=\"debian\"" ]]; then
-		printf '\nTODO\n'
-	fi
-	printf '\nTODO\n'
-fi
-
-
-
-
-
-#audit sources/mirrors
-if [[ "$option" = "18" ]]; then
-	if [[ ${ID_LIKE} = "ID_LIKE=rhel fedora" ]] || [[ ${ID_LIKE} = "ID_LIKE=\"rhel fedora\"" ]]; then
-		printf '\nTODO\n'
-	fi
-
-	if [[ ${ID_LIKE} = "ID_LIKE=arch" ]] || [[ ${ID_LIKE} = "ID_LIKE=\"arch\"" ]]; then
-		printf '\nTODO\n'
-	fi
-
-	if [[ ${ID_LIKE} = "ID_LIKE=debian" ]] || [[ ${ID_LIKE} = "ID_LIKE=\"debian\"" ]]; then
-		printf '\nTODO\n'
-	fi
-	printf '\nTODO\n'
-fi
-
-#ends while loop
 done
