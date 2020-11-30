acbce14466dfd09cbeb3a8a376bd6eb4429a6871
Nathan Melwani <melwaninate@gmail.com>
2020-11-29 22:48:02 -0800

    Added ipv6/iptables installation/debugging messages

--- blue-team/linux/iptables.sh
+++ blue-team/linux/iptables.sh
@@ -7,18 +7,31 @@ fi
 
 echo Ll9fICAgICAgICBfXyAgICAgICAgX19fLiAgIC5fXyAgICAgICAgICAgICAgICAgCnxfX3xfX19fX18vICB8X19fX19fIFxfIHxfXyB8ICB8ICAgX19fXyAgIF9fX19fXwp8ICBcX19fXyBcICAgX19cX18gIFwgfCBfXyBcfCAgfCBfLyBfXyBcIC8gIF9fXy8KfCAgfCAgfF8+ID4gIHwgIC8gX18gXHwgXF9cIFwgIHxfXCAgX19fLyBcX19fIFwgCnxfX3wgICBfXy98X198IChfX19fICAvX19fICAvX19fXy9cX19fICA+X19fXyAgPgogICB8X198ICAgICAgICAgICAgIFwvICAgIFwvICAgICAgICAgIFwvICAgICBcLyAK | base64 -d
 
+sudo apt-get install iptables -y
+
+echo Setting Timezone to America/Los_Angeles
 sudo timedatectl set-timezone America/Los_Angeles
 sudo systemctl restart rsyslog
 
+echo Setting Up IPV4
 sudo iptables -F
 sudo iptables -A INPUT -p tcp --dport 23 -j DROP
 sudo iptables -X LOGGING
 sudo iptables -N LOGGING
 sudo iptables -A INPUT -j LOGGING
-sudo iptables -A LOGGING -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j LOG --log-prefi>
-sudo iptables -I OUTPUT -p tcp -m tcp --sport 22 -m state --state NEW,ESTABLISHED -j LOG --log>
+sudo iptables -A LOGGING -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j LOG --log-prefix "Incoming SSH Connection: " --log-level 7
+sudo iptables -I OUTPUT -p tcp -m tcp --sport 22 -m state --state NEW,ESTABLISHED -j LOG --log-prefix "Outgoing SSH connection: " --log-level 7
 sudo iptables -A LOGGING -j ACCEPT
 
-echo Done!
+echo Setting Up IPV6
+sudo ip6tables -F
+sudo ip6tables -X
+sudo ip6tables -P INPUT DROP
+sudo ip6tables -P OUTPUT DROP
+sudo ip6tables -P FORWARD DROP
+
+
+
+echo Script Done!
 
 exit
