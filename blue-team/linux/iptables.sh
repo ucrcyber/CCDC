34a38249a4161287d5d7f61fd9cfdaf72663869d
Nathan Melwani <melwaninate@gmail.com>
2020-11-29 22:26:54 -0800

    Update iptables.sh

--- blue-team/linux/iptables.sh
+++ blue-team/linux/iptables.sh
@@ -5,7 +5,7 @@ if [ `whoami` != "root" ]; then
         exit 1
 fi
 
-echo Ll9fICAgICAgICBfXyAgICAgICAgX19fLiAgIC5fXyAgICAgICAgICAgICAgICAgCnxfX3xfX19fX18vICB8X19fX>
+echo Ll9fICAgICAgICBfXyAgICAgICAgX19fLiAgIC5fXyAgICAgICAgICAgICAgICAgCnxfX3xfX19fX18vICB8X19fX19fIFxfIHxfXyB8ICB8ICAgX19fXyAgIF9fX19fXwp8ICBcX19fXyBcICAgX19cX18gIFwgfCBfXyBcfCAgfCBfLyBfXyBcIC8gIF9fXy8KfCAgfCAgfF8+ID4gIHwgIC8gX18gXHwgXF9cIFwgIHxfXCAgX19fLyBcX19fIFwgCnxfX3wgICBfXy98X198IChfX19fICAvX19fICAvX19fXy9cX19fICA+X19fXyAgPgogICB8X198ICAgICAgICAgICAgIFwvICAgIFwvICAgICAgICAgIFwvICAgICBcLyAK | base64 -d
 
 sudo timedatectl set-timezone America/Los_Angeles
 sudo systemctl restart rsyslog
