d7b3c36828e237a49513d6f927e1ec066fc6db74
Adil Mohiuddin <amohi9046@gmail.com>
2022-02-05 13:16:19 -0800

    backup: removed mysql

--- blue-team/linux/backup.sh
+++ blue-team/linux/backup.sh
@@ -21,16 +21,6 @@ cp -r /lib/security* /var/backup
 becho "Backing up configuration files from /etc"
 cp -r /etc /var/backup
 
-if [ -d "/var/lib/mysql"]
-then
-	becho "Backing up mysql"
-	# Info for restoring: https://serverfault.com/questions/659048/can-i-copy-the-entire-var-lib-mysql-folder-to-a-different-server-mysql-vs-mar
-	cp -r /var/lib/mysql /var/backup
-
-	# Alternative
-	# mysqldump --all-databases --single-transaction --quick --lock-tables=false > /var/backups/mysql_full-backup-$(date +%F).sql -u root -p
-fi
-
 if [ -d "/var/www" ]; then
 	becho "Backing up web files"
 	cp -r /var/www /var/backup
