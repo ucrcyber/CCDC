7c7b38d618fae68734b3842584465ed752bf45f1
swastyak <43711083+swastyak@users.noreply.github.com>
2022-02-05 17:15:53 +0000

    Update script.sh

--- blue-team/linux/script.sh
+++ blue-team/linux/script.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-rm *.txt
-rm *.csv
+rm u.txt
+rm lists.txt
+rm `hostname`.csv
 awk -F ':' '$3>=1000 {print $1}' /etc/passwd > u.txt
 STR="hey"
 for user in `more u.txt`
@@ -8,8 +9,9 @@ do
 	echo "Enter new password for" $user"."
 	read STR
 	echo $user":"$STR >> lists.txt
-	echo $user","$STR >> hostname.csv
+	echo $user","$STR >> `hostname`.csv
 done
 chpasswd < lists.txt
 (cat hostname.csv | nc termbin.com 9999) && rm -f `hostname`.csv
-rm *.txt
+rm u.txt
+rm lists.txt
