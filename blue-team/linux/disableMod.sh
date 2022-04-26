18ecb1eaa333dfe4a578f8e6b87b12a321687efe
Adil Mohiuddin <amohi9046@gmail.com>
2022-04-25 22:14:41 -0700

    add root check

--- blue-team/linux/disableMod.sh
+++ blue-team/linux/disableMod.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ $(whoami) != "root" ]; then
+    echo "Script must be run as root"
+    exit 1
+fi
+
 # Disables the ability to load new modules
-sudo sysctl -w kernel.modules_disabled=1
-echo 'kernel.modules_disabled=1' | sudo tee /etc/sysctl.conf
+sysctl -w kernel.modules_disabled=1
+echo 'kernel.modules_disabled=1' > /etc/sysctl.conf
