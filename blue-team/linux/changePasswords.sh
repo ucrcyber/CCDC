01a7aa2725d118928afb4773894bd4f4a44f0069
Adil Mohiuddin <amohi9046@gmail.com>
2022-04-25 22:07:44 -0700

    update passwd script

--- blue-team/linux/changePasswords.sh
+++ blue-team/linux/changePasswords.sh
@@ -1,29 +1,15 @@
 #!/bin/sh
-rm u.txt
-rm lists.txt
-rm `hostname`.csv
-awk -F ':' '$3>=1000 {print $1}' /etc/passwd > u.txt
-STR="hey"
 
-for user in `more u.txt`
+if [ $(whoami) != "root" ]; then
+    echo "Script must be run as root"
+    exit 1
+fi
+
+echo "Enter password template"
+read STR
+
+for user in $(awk -F: '$7 ~ /(\/.*sh)/ { print $1 }' /etc/passwd)
 do
-	echo "Enter new password for" $user"."
-	read STR
-	echo $user":"$STR >> lists.txt
-	echo $user","$STR >> `hostname`.csv
+    echo "${user},${STR}${user}"
+    echo "${user}:${STR}${user}" | chpasswd
 done
-
-chpasswd < lists.txt
-
-# prompt user for server info
-read -p "Please enter the server IP: " serverIP
-read -p "Please enter your username: " username 
-
-# make backup folder if it doesn't exist
-ssh "$username@$serverIP" mkdir -p "/home/$username/backup"
-# call scp command on server’s backup folder
-scp `hostname`.csv "$username@$serverIP:/home/backup"
-
-rm `hostname`.csv
-rm u.txt
-rm lists.txt
