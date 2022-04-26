80bb473032f895876c9a4d001cedc6d1feffc73e
Adil Mohiuddin <amohi9046@gmail.com>
2022-04-25 22:10:18 -0700

    update backup script

--- blue-team/linux/backup.sh
+++ blue-team/linux/backup.sh
@@ -11,20 +11,25 @@ becho() {
 	echo "$(tput bold)$1...$(tput sgr0)"
 }
 
-# Create backups directory 
+# Create backups directory
 mkdir -p /var/backups
 
 becho "Backing up pam"
 cp -r /etc/pam* /var/backups
-cp -r /lib/security* /var/backup
+cp -r /lib/security* /var/backups
 
 becho "Backing up configuration files from /etc"
 cp -r /etc /var/backup
 
 if [ -d "/var/www" ]; then
 	becho "Backing up web files"
-	cp -r /var/www /var/backup
+	cp -r /var/www /var/backups
+fi
+
+if [ -d "/var/lib/mysql" ]; then
+	becho "Backing up MySQL"
+	mysqldump -u root -p --all-databases > /var/backups/mysql.sql
 fi
 
 becho "Making backups immutable"
-chattr +i -RV /var/backups/*
+chattr +i -R /var/backups/*
